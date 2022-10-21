cb7591806ba5bdb56a6ec839d7e19c818ed3f580
Alumno20 b202 <a20b202@campustls.local>
2022-10-21 09:06:19 -0500

    añadir vida

---
+++ Assets/Scripts/life.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class life : MonoBehaviour
+{
+    public float vida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (vida <= 0)
+        {
+            Debug.Log("");
+        }
+    }
+}
---
+++ Assets/Scripts/move_player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_player : MonoBehaviour
+{
+    public float force = 150;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 vector = new Vector3(x, 0.5f, y);
+        rb.AddForce(vector * force * Time.deltaTime);
+    }
+}
