ef69af0a47d68cbd73b86bd919a44ad843eafb2d
Dario Reque <dario.reque.17@gmail.com>
2022-10-22 00:00:11 -0500

    Se termino el Enemy_Folow

--- Assets/Scripts/Enemy_Folow.cs
+++ Assets/Scripts/Enemy_Folow.cs
@@ -8,11 +8,28 @@ public class Enemy_Folow : MonoBehaviour
     public Transform Player;
     public float Speed;
     public NavMeshAgent Enemy;
+    public LayerMask whatIsPlayer;
+
+    public float sightRange;
+    public bool playerInSightRange;
 
     void Update()
     {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if (playerInSightRange) Follow();
+        if (!playerInSightRange) StopFollow();
+
+    }
+
+    public void Follow()
+    {
         Enemy.speed = Speed;
         Enemy.SetDestination(Player.position);
+    }
 
+    public void StopFollow()
+    {
+        Enemy.speed = 0;
     }
 }
